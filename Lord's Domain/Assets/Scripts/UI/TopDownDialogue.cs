f1ae0d0c6ff1f77e5041fe22f94d34aa688fb0c5
黄龙 <437038105@qq.com>
2021-04-05 19:10:14 -0500

    initial dialogue system implemented

---
+++ Lord's Domain/Assets/Scripts/UI/TopDownDialogue.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownDialogue : MonoBehaviour
+{
+    private bool dialogue_flag = false;
+    private GameObject closet_ob;
+    private float closet_distance;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!dialogue_flag)
+            {
+                (closet_ob, closet_distance) = FindClosestEnemy();
+                Debug.Log("Distance:" + closet_distance);
+                if (closet_distance < 3)
+                {
+                    DialogueTrigger script1 = closet_ob.GetComponent<DialogueTrigger>();
+                    script1.TriggerDialogue();
+                    dialogue_flag = true;
+                }
+            }
+            else
+            {
+                DialogueManager script2 = GetComponent<DialogueManager>();
+                if (script2.DisplayNextSentence())
+                {
+                    dialogue_flag = false;
+                }
+            }
+        }
+    }
+    
+    public (GameObject, float) FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return (closest, distance);
+    }
+}
