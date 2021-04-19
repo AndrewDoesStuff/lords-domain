417f264f36dff3d993e5dabd353a3634e33c2631
Andrew Hannie <ahhannie56@gmail.com>
2021-04-18 23:49:38 -0500

    added more interactables

--- LordsDomain/Assets/Scripts/Runner MG/Player.cs
+++ LordsDomain/Assets/Scripts/Runner MG/Player.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public float speed = 7.5f;
-    public Rigidbody2D rb;
-    public int health = 3;
+    private Rigidbody2D rb;
+    public int health = 5;
     private Vector2 movement;
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
